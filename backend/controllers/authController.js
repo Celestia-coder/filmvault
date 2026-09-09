3bd8f6862cb61d74a3c020686a847e8f4834f75c
Ralph <ralphwendelf@gmail.com>
2026-09-09 21:29:12 +0800

    Setup Structure

---
+++ backend/controllers/authController.js
@@ -0,0 +1,12 @@
+const register = (req, res) => {
+  res.status(501).json({ message: "register not implemented yet" });
+};
+
+const login = (req, res) => {
+  res.status(501).json({ message: "login not implemented yet" });
+};
+
+module.exports = {
+  register,
+  login,
+};
